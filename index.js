2d4ec2b5eaade13f1708cfd5044817870a5cea3f
A-Farabi <farabisyl2017@gmail.com>
2025-02-08 19:42:32 +0600

    insert op done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrtaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // middleware
 app.use(cors())
@@ -12,10 +13,6 @@ app.use(express.json())
 app.get('/', (req, res)=>{
     res.send('Visa Navigator server is running')
 })
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrtaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const visaCollection = client.db("visaDB").collection('visa')
+
+    app.post('/Visa', async (req, res)=>{
+      const newAddingVisa = req.body
+      const result = await visaCollection.insertOne(newAddingVisa)
+      res.send(result)
+    })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
